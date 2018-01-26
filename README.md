a3864229b663cc21e20e05f527c2793541e8d358
Shashank Kumar Shankar <shashank.kumar.shankar@intel.com>
2018-01-26 14:25:32 -0800

    Initial seed commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# consul-default-kv-microservice
---
+++ main.go
@@ -0,0 +1,9 @@
+package consul_default_kv_microservice
+
+import (
+	api "github.com/shank7485/consul-default-kv-microservice/api"
+)
+
+func main()  {
+	api.SimpleGet()
+}
